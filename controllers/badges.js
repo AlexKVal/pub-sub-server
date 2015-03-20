22ac2b74102564e4a9016e91e863ae4fa0729aff
AlexKVal <alexkval@gmail.com>
2015-03-20 18:43:18 +0200

    add badges model

--- controllers/badges.js
+++ controllers/badges.js
@@ -1,12 +1,23 @@
 'use sctrict';
 
-function save(req, res) {
-  res.send('hello from save');
-}
+var _ = require('underscore');
+var model = require('../models/badges');
 
-function send(req, res) {
+/**
+ * Send badges to model to be saved
+ */
+exports.save = function(req, res, next) {
+  var badges = _.clone(req.body);
+
+  model.save(badges, function (err) {
+    if(err) return res.json(503, { error: true });
+    next();
+  });
+};
+
+/**
+ * Send badges to pub/sub socket in model
+ */
+exports.send = function(req, res) {
   res.send('hello from send');
-}
-
-module.exports.save = save;
-module.exports.send = send;
+};
